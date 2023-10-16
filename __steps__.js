998771e5fd6887288f5cebdcbd3b50b1226381d4
Yousufjoy <kanekiken333@gmail.com>
2023-10-16 21:02:56 +0600

    Send Client data to the server and display data on the client

--- __steps__.js
+++ __steps__.js
@@ -1,6 +1,9 @@
 /**
  * 1. create a post api on the server side
  * 2. Client side send data via post
- *
- *
+ * 3. set fetch method inside the fetch options(Second parameters) {https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch}
+ * 4. options will have three things : method
+ * 5. options will have headers: "content-type": "application/json"
+ * 6. don't forget to send data by JSON.Stringify() in the body
+ * 7. On the server side: dont forget to use express.json() middleware {cause server e eshe shei string object k parse kore normal object e convert kortesi!}
  */
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleWare
 app.use(cors());
+app.use(express.json());
 
 const users = [
   {
@@ -37,8 +38,11 @@ app.get("/users", (req, res) => {
 // Ami kon typer jinis paite chai seta jodi na mention kore dei tahole default hisebe get er kase dhore fele r put,post,delete er khetre explicitly bole dite hobe client side theke
 app.post("/users", (req, res) => {
   console.log("Post api hitting!");
-  const newUser = req.body;
-  console.log(newUser);
+  console.log(req.body);
+  const newUser = req.body; // Client side theke ekhane data ta set hocche
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 // Chalanor jonno
